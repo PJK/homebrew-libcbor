fbfa62f9133a5072d352317d2640414d0ea259b1
PJK <me@pavelkalvoda.com>
2016-04-19 00:15:47 +0200

    Test & description

--- libcbor.rb
+++ libcbor.rb
@@ -1,37 +1,36 @@
-# Documentation: https://github.com/Homebrew/brew/blob/master/share/doc/homebrew/Formula-Cookbook.md
-#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class Libcbor < Formula
-  desc ""
-  homepage ""
+  desc "CBOR format implementation for C"
+  homepage "http://libcbor.org/"
   url "https://github.com/PJK/libcbor/archive/v0.4.0.tar.gz"
   version "0.4.0"
   sha256 "1b61e6eba2b5b18b5ec1a29161ad7037689464e77a4ad6cc1c084f888b531acf"
 
   depends_on "cmake" => :build
-  depends_on :x11 # if your formula requires any X11/XQuartz components
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-
    	mkdir "build" do
 	    system "cmake", "-G", "Unix Makefiles", "..", *std_cmake_args
 	    system "make"
 	    system "make", "install"
- 	end
+ 	  end
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test libcbor`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    (testpath/"example.c").write <<-EOS.undent
+      #include "cbor.h"
+      #include <stdio.h>
+
+      int main(int argc, char * argv[])
+      {
+        printf("Hello from libcbor %s\\n", CBOR_VERSION);
+        printf("Custom allocation support: %s\\n", CBOR_CUSTOM_ALLOC ? "yes" : "no");
+        printf("Pretty-printer support: %s\\n", CBOR_PRETTY_PRINTER ? "yes" : "no");
+        printf("Buffer growth factor: %f\\n", (float) CBOR_BUFFER_GROWTH);
+      }
+    EOS
+
+    system "cc -std=c99 #{(testpath/"example.c")} -lcbor -o example"
+    system testpath/"example"
+    puts `#{testpath/"example"}`
   end
 end
